266361f912acc4e84c6aeb1265985be1879478fe
Aiden Dumas <aidendumas12345@gmail.com>
2026-01-21 12:52:45 -0500

    Added backup location

--- run.sh
+++ run.sh
@@ -2,11 +2,12 @@
 java \
   -Xms4G \
   -Xmx8G \
-  -XX:AOTCache=Server/HytaleServer.aot \
+  -XX:AOTCache=/hytale/Server/HytaleServer.aot \
   -XX:+UseG1GC \
   -XX:+ParallelRefProcEnabled \
   -XX:MaxGCPauseMillis=200 \
-  -jar Server/HytaleServer.jar \
-  --assets Server/Assets.zip \
+  -jar /hytale/Server/HytaleServer.jar \
+  --assets /hytale/Assets.zip \
   --backup \
+  --backup-dir /hytale/backups \
   --backup-frequency 30
