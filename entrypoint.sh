4b2b8e5d2608447256e0a175ddc63ddeb1c48c0d
Aiden Dumas <aidendumas12345@gmail.com>
2026-01-24 10:10:59 -0500

    Fixed start script execution perms and stalling issue

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,20 +8,19 @@ if [ ! -f "$FIRST_RUN_FLAG" ]; then
     echo "First run detected — running setup..."
 
     # Install hytale server files
-    wget https://downloader.hytale.com/hytale-downloader.zip && \
-    unzip hytale-downloader.zip hytale-downloader-linux-amd64 && \
-    rm /hytale/hytale-downloader.zip && \
-    chmod +x /hytale/hytale-downloader-linux-amd64 && \
-    ./hytale-downloader-linux-amd64 --download-path /hytale/game.zip && \
-    unzip -o /hytale/game.zip -d /hytale && \
+    wget https://downloader.hytale.com/hytale-downloader.zip
+    unzip -o hytale-downloader.zip hytale-downloader-linux-amd64
+    rm /hytale/hytale-downloader.zip
+    chmod +x /hytale/hytale-downloader-linux-amd64
+    ./hytale-downloader-linux-amd64 --download-path /hytale/game.zip
+    unzip -o /hytale/game.zip -d /hytale
     rm /hytale/game.zip
 
-    touch "$FIRST_RUN_FLAG"    
-    game_version=$(./hytale-downloader-linux-amd64 -print-version)
-    downloader_version=$(./hytale-downloader-linux-amd64 -version)
+    chmod +x /start.sh
 
-    echo "$game_version" > /hytale/version.txt
-    echo "$downloader_version" >> /hytale/version.txt
+    touch "$FIRST_RUN_FLAG"    
+
+    echo "Setup complete."
 fi
 
 echo "Starting main app..."
